78ae5e5dc6660a3451f777ddf1d81a53587003bf
Анна Баулина <annika10@list.ru>
2020-10-15 11:27:32 +0300

    button start + pdfSaver lib

--- GlossaryTermApp/StartPage.xaml.cs
+++ GlossaryTermApp/StartPage.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace GlossaryTermApp
 {
@@ -24,9 +13,13 @@ namespace GlossaryTermApp
             InitializeComponent();
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        private void ButtonStart_OnClick(object sender, RoutedEventArgs e)
         {
-           
+            MainWindow mainWindow=new MainWindow();
+            this.Hide();
+            mainWindow.Show();
+            this.Close();
         }
+
     }
 }
---
+++ PdfSaver/Class.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using IronPdf;
+
+namespace PdfSaver
+{
+    public class Export
+    {
+        public Export(){}
+        public void CaptureScreen()
+        {
+            var srcimage = ScreenCapture.CaptureActiveWindow();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "pdf files (*.pdf)|*.pdf";
+            saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments);
+            saveFileDialog.RestoreDirectory = true;
+                DialogResult result = saveFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string fileName = saveFileDialog.FileName;
+                ImageToPdfConverter.ImageToPdf(srcimage,ImageBehavior.TopLeftCornerOfPage,true).SaveAs(Path.Combine(fileName));
+            }
+        }
+    }
+
+    public class ScreenCapture
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetDesktopWindow();
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        public static Bitmap CaptureActiveWindow()
+        {
+            return CaptureWindow(GetForegroundWindow());
+        }
+
+        public static Bitmap CaptureWindow(IntPtr handle)
+        {
+            var rect = new Rect();
+            GetWindowRect(handle, ref rect);
+            var bounds = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            var result = new Bitmap(bounds.Width, bounds.Height);
+
+            using (var graphics = Graphics.FromImage(result))
+            {
+                graphics.CopyFromScreen(new Point(bounds.Left, bounds.Top), Point.Empty, bounds.Size);
+            }
+
+            return result;
+        }
+    }
+}
