eaca38ad64796820fc1a300d7f220ce1a7e3bab0
Анна Баулина <annika10@list.ru>
2020-10-15 18:02:07 +0300

    internal subject & grade

--- GlossaryTermApp/StartPage.xaml.cs
+++ GlossaryTermApp/StartPage.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Controls;
+using SerializerLib;
 
 namespace GlossaryTermApp
 {
@@ -13,13 +16,26 @@ namespace GlossaryTermApp
             InitializeComponent();
         }
 
+        internal int grade;
+        internal string subject;
         private void ButtonStart_OnClick(object sender, RoutedEventArgs e)
         {
+            bool okayToContinue = false;
+            while (okayToContinue==false)
+            {
+                okayToContinue = true;
+                string input = ComboBoxGrade.Text;
+                Regex regex = new Regex(@"(\d)+\D");
+                grade = int.Parse(regex.Match(input).ToString());
+                subject = ComboBoxSubject.Text;
+            }
             MainWindow mainWindow=new MainWindow();
             this.Hide();
             mainWindow.Show();
             this.Close();
         }
 
+        private void SelectionCheck()
+        {}
     }
 }
