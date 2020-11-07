c07891ad60a4c85133d16e2f39cc6cb02d66ea39
Анна Баулина <annika10@list.ru>
2020-11-07 16:05:55 +0300

    Visual HitTest (не работает)

--- GlossaryTermApp/MatchGamePage.xaml.cs
+++ GlossaryTermApp/MatchGamePage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using MatchGameLib;
@@ -80,7 +82,8 @@ namespace GlossaryTermApp
                     BorderThickness = new Thickness(2),
                     BorderBrush = new SolidColorBrush(Color.FromRgb(202, 207, 210)),
                     Margin = new Thickness(10, 10, 0, 0),
-                    Child = wordTextBlock
+                    Child = wordTextBlock,
+                    Tag = wordTextBlock.Tag
                 };
                 forTextBlock.MouseDown += WordTextBlock_MouseDown;
                 forTextBlock.MouseUp += WordTextBlock_MouseUp;
@@ -90,17 +93,49 @@ namespace GlossaryTermApp
         }
 
         private Border _senderTextBlock;
+        private Rectangle _hitRectangle=new Rectangle();
         private void WordTextBlock_MouseMove(object sender, MouseEventArgs e)
         {
             if (_captured)
             {
+                _hitRectangle = new Rectangle();
                 _senderTextBlock = (Border) sender;
                 Point mousePos = e.GetPosition(this);
                 double newX = mousePos.X - DragOffset.X;
                 double newY = mousePos.Y - DragOffset.Y;
-                _senderTextBlock.RenderTransform = new TranslateTransform(newX,newY);
-                _senderTextBlock.RenderTransformOrigin = new Point(newX,newY);
+                _senderTextBlock.RenderTransform = new TranslateTransform(newX, newY);
+                _senderTextBlock.RenderTransformOrigin = new Point(newX, newY);
+
+                VisualTreeHelper.HitTest(ForStackPanelScrollViewer, null /*HitFilter*/, HitResult,
+                    new PointHitTestParameters(new Point(newX, newY)));
+                //HitTestResult result = VisualTreeHelper.HitTest(ForStackPanelScrollViewer, new Point(newY, newY));
+
+                if (_hitRectangle != null)
+                {
+                    if (_hitRectangle.Tag == _senderTextBlock.Tag)
+                    {
+                        _senderTextBlock.Tag = "true";
+                    }
+                }
             }
+        }
+
+        //HitTestFilterBehavior HitFilter(DependencyObject o)
+        //{
+        //    if (!(o is Rectangle)) return HitTestFilterBehavior.ContinueSkipChildren;
+        //    return HitTestFilterBehavior.Continue;
+        //}
+        //docs.microsoft.com/ru-ru/dotnet/api/system.windows.media.visualtreehelper.hittest?redirectedfrom=MSDN&view=netcore-3.1#System_Windows_Media_VisualTreeHelper_HitTest_System_Windows_Media_Visual_System_Windows_Media_HitTestFilterCallback_System_Windows_Media_HitTestResultCallback_System_Windows_Media_HitTestParameters_
+
+        HitTestResultBehavior HitResult(HitTestResult result)
+        {
+            if (result.VisualHit is Rectangle)
+            {
+                _hitRectangle = result.VisualHit as Rectangle;
+                return HitTestResultBehavior.Stop;
+            }
+
+            return HitTestResultBehavior.Continue;
         }
 
         private void WordTextBlock_MouseUp(object sender, MouseButtonEventArgs e)
@@ -118,5 +153,17 @@ namespace GlossaryTermApp
             _senderTextBlock.CaptureMouse();
         }
 
+        private void CheckBTN_OnClick(object sender, RoutedEventArgs e)
+        {
+            var countTrue = 0;
+            foreach (var child in WordsWrapPanel.Children)
+            {
+                var termWithBorder = (Border) child;
+                if (termWithBorder.Tag == "true")
+                {
+                    countTrue++;
+                }
+            }
+        }
     }
 }
