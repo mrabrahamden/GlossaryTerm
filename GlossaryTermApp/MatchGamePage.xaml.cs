cca02c389372424d6a85a92cfe6d48b22dadcae3
Анна Баулина <annika10@list.ru>
2020-11-08 17:16:54 +0300

    галочки-крестики в деле, MatchGame is ready

--- GlossaryTermApp/MatchGamePage.xaml.cs
+++ GlossaryTermApp/MatchGamePage.xaml.cs
@@ -1,25 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
 using MatchGameLib;
 using TermLib;
 using Color = System.Windows.Media.Color;
-using Rectangle = System.Windows.Shapes.Rectangle;
 
 namespace GlossaryTermApp
 {
-    /// <summary>
-    /// Логика взаимодействия для MatchGamePage.xaml
-    /// </summary>
     public partial class MatchGamePage : Window
     {
-        private bool _captured;
-        private Point DragOffset;
         public MatchGame MatchGame;
         public MatchGamePage(MatchGame matchGame)
         {
@@ -27,17 +23,30 @@ namespace GlossaryTermApp
             MatchGame = matchGame;
             PrepareForm();
         }
+        private ImageBrush GetBrushFromImage(string path)
+        {
+            var image = System.Drawing.Image.FromFile(path);
+            var bitmap = new System.Drawing.Bitmap(image);
+            var bitmapSource = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(),
+                IntPtr.Zero,
+                Int32Rect.Empty,
+                BitmapSizeOptions.FromEmptyOptions()
+            );
+            bitmap.Dispose();
+            var brush = new ImageBrush(bitmapSource);
+            return brush;
+        }
 
-        private StackPanel WordsStackPanel;
-        private List<TextBlock> listTextBlocksForCheck;
-        private List<Border> listOfWordBorders;
+        private StackPanel _wordsStackPanel;
+        private List<TextBlock> _listTextBlocksForCheck;
+        private List<Border> _listOfWordBorders;
         private void PrepareForm()
         { 
-            WordsStackPanel = new StackPanel();
-            listTextBlocksForCheck=new List<TextBlock>();
-            listOfWordBorders=new List<Border>();
-            WordsStackPanel.Background = (SolidColorBrush) new BrushConverter().ConvertFromString("#F2F3F4");
-            ForStackPanelScrollViewer.Content = WordsStackPanel;
+            _wordsStackPanel = new StackPanel();
+            _listTextBlocksForCheck=new List<TextBlock>();
+            _listOfWordBorders=new List<Border>();
+            _wordsStackPanel.Background = (SolidColorBrush) new BrushConverter().ConvertFromString("#F2F3F4");
+            ForStackPanelScrollViewer.Content = _wordsStackPanel;
             foreach (var term in MatchGame.TermList)
             {
                 StackPanel termStackPanel=new StackPanel()
@@ -54,7 +63,7 @@ namespace GlossaryTermApp
                     Text = term.Description,
                     FontSize = 20,
                     Tag = term,
-                    TextAlignment = TextAlignment.Center,
+                    TextAlignment = TextAlignment.Left,
                     Background = (SolidColorBrush) new BrushConverter().ConvertFromString("#F9F7A8"),
                     TextWrapping = TextWrapping.Wrap,
                     Padding = new Thickness(5, 2, 5, 2),
@@ -66,20 +75,26 @@ namespace GlossaryTermApp
                     FontSize = 18,
                     Tag = term,
                     Background = new SolidColorBrush(Color.FromRgb(202, 207, 210)),
-                    Margin = new Thickness(30, 10, 10, 0),
+                    Margin = new Thickness(15, 10, 10, 0),
                     Width = 200,
                     TextAlignment = TextAlignment.Center,
                     VerticalAlignment = VerticalAlignment.Center
                 };
-                listTextBlocksForCheck.Add(placeForWordTextBlock);
-                //placeForWordCanvas.DragEnter += PlaceForWordRectangle_DragEnter;
+                _listTextBlocksForCheck.Add(placeForWordTextBlock);
                 placeForWordTextBlock.Drop += PlaceForWordRectangle_Drop;
                 placeForWordTextBlock.MouseDown += PlaceForWordTextBlock_MouseDown;
                 wrapPanel.Children.Add(descriptionTextBlock);
+                TextBlock correctOrNotSign = new TextBlock()
+                {
+                    Margin = new Thickness(10,10,0,0),
+                    Height = 22,
+                    Width = 22
+                };
                 termStackPanel.Children.Add(placeForWordTextBlock);
+                dockPanel.Children.Add(correctOrNotSign);
                 dockPanel.Children.Add(termStackPanel);
                 dockPanel.Children.Add(wrapPanel);
-                WordsStackPanel.Children.Add(dockPanel);
+                _wordsStackPanel.Children.Add(dockPanel);
 
                 TextBlock wordTextBlock = new TextBlock()
                 {
@@ -101,10 +116,10 @@ namespace GlossaryTermApp
 
                 forTextBlock.MouseDown += ForTextBlock_MouseDown;
 
-                listOfWordBorders.Add(forTextBlock);
+                _listOfWordBorders.Add(forTextBlock);
             }
             ShuffleList();
-            foreach (var border in listOfWordBorders)
+            foreach (var border in _listOfWordBorders)
             {
                 WordsWrapPanel.Children.Add(border);
             }
@@ -113,7 +128,7 @@ namespace GlossaryTermApp
         private void PlaceForWordTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
         {
             var textblock = (TextBlock) sender;
-            foreach (var border in listOfWordBorders)
+            foreach (var border in _listOfWordBorders)
             {
                 var term = (SimpleTerm)border.Tag;
                 if (term.Word == textblock.Text)
@@ -128,20 +143,20 @@ namespace GlossaryTermApp
         {
             List<Border> newList=new List<Border>();
             Random random=new Random(DateTime.Now.Millisecond);
-            while(listOfWordBorders.Count>0)
+            while(_listOfWordBorders.Count>0)
             {
-                int count= random.Next() % listOfWordBorders.Count;
-                newList.Add(listOfWordBorders[count]);
-                listOfWordBorders.RemoveAt(count);
+                int count= random.Next() % _listOfWordBorders.Count;
+                newList.Add(_listOfWordBorders[count]);
+                _listOfWordBorders.RemoveAt(count);
             }
-            listOfWordBorders=new List<Border>(newList);
+            _listOfWordBorders=new List<Border>(newList);
         }
         private void PlaceForWordRectangle_Drop(object sender, DragEventArgs e)
         {
             var textblock = (TextBlock) sender;
             if (textblock.Text.Length > 0)
             {
-                foreach (var border in listOfWordBorders)
+                foreach (var border in _listOfWordBorders)
                 {
                     var term = (SimpleTerm)border.Tag;
                     if (term.Word == textblock.Text)
@@ -150,7 +165,7 @@ namespace GlossaryTermApp
             }
             textblock.Text = (string)e.Data.GetData(DataFormats.Text);
             textblock.Background = (SolidColorBrush) new BrushConverter().ConvertFromString("#F08080");
-            foreach (var border in listOfWordBorders)
+            foreach (var border in _listOfWordBorders)
             {
                 var term = (SimpleTerm) border.Tag;
                 if (term.Word == textblock.Text)
@@ -165,26 +180,27 @@ namespace GlossaryTermApp
             DragDrop.DoDragDrop(border,textblock.Text, DragDropEffects.Copy);
         }
 
-        //private void PlaceForWordRectangle_DragEnter(object sender, DragEventArgs e)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
         private void CheckBTN_OnClick(object sender, RoutedEventArgs e)
         {
             var countTrue = 0;
-            foreach (var textBlock in listTextBlocksForCheck)
+            foreach (var textBlock in _listTextBlocksForCheck)
             {
                 var term = (SimpleTerm) textBlock.Tag;
-                if ( term.Word == textBlock.Text)
+                var smallStackPanel = (StackPanel)textBlock.Parent;
+                var dockPanel = (DockPanel) smallStackPanel.Parent;
+                var correctOrNotSign = (TextBlock) dockPanel.Children.OfType<TextBlock>().First();
+                if (term.Word == textBlock.Text)
                 {
                     countTrue++;
                     textBlock.Background = Brushes.LightGreen;
+                    correctOrNotSign.Background = GetBrushFromImage("image/true.png");
                 }
                 else
                 {
                     textBlock.Background = Brushes.LightCoral;
+                    correctOrNotSign.Background = GetBrushFromImage("image/false.png");
                 }
+                
             }
 
             if (!MatchGame.TrainingMode)
