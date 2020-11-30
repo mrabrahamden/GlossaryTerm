b816ae52ac693d23292bab95ac26755b7eca83e8
Анна Баулина <annika10@list.ru>
2020-11-30 18:55:13 +0300

    18 окончательно

--- GlossaryTermApp/MainWindow.xaml.cs
+++ GlossaryTermApp/MainWindow.xaml.cs
@@ -403,7 +403,7 @@ namespace GlossaryTermApp
 
         private void MatchGameStartBTN_Click(object sender, RoutedEventArgs e)
         {
-            if (Serializer.TermList.Count > 0)
+            if (Serializer.TermList.Count > 2)
             {
                 _matchGame = new MatchGame(Serializer.TermList, (int)MatchGameCountUpDown.Value,
                     (bool)MatchGameTrainingMode.IsChecked);
