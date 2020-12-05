06114b512b282a3fa2182beb46eafba5eb371315
Alex Levshinskiy <leshka00.00@live.com>
2020-12-05 20:36:35 +0300

    names of libs

--- GlossaryTermApp/Properties/Resources.Designer.cs
+++ GlossaryTermApp/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace TeacherryApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GlossaryTermApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TeacherryApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
